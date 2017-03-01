107c1a083eaebe54027a6a0fc98857f695ab3d37
dell-pc <dell-pc@193.1.164.246>
2017-03-01 17:13:26 +0000

    df

---
+++ computer science/semester2/COMP30670 Software Engineering (Conversion) 2016-2017/java/assi/setup.py
@@ -0,0 +1,22 @@
+'''
+Created on Mar 1, 2017
+
+@author: dell-pc
+'''
+from setuptools import setup
+
+setup(name="assi",
+      version="0.1",
+      description="LED Testing for Assignment3 in COMP30670 2017",
+      url="",
+      author="Peng Xu",
+      author_email="aonghus.lawlor@ucd.ie",
+      licence="GPL3",
+      packages=['NAME'],
+      entry_points={
+        'console_scripts':['led_tester=NAME.main:main']
+        },
+      install_requires=[
+          'numpy',
+      ],
+      )
